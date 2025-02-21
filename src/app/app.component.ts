94e3a9f6793f67551b55832c5f6a9114359c3675
Mariana <mrqsp.rg@gmail.com>
2025-02-21 11:26:16 -0300

    Conceitos básicos do Angular com Indexa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './componentes/container/container.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
+
+interface Contato {
+  id: number,
+  nome: string,
+  telefone: string
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    ContainerComponent, 
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule
+],  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda;
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto (): Contato [] {
+    if (!this.filtroPorTexto) {
+        return this.contatos
+  }
+      return this.contatos.filter(contato => {
+          return contato.nome.toLowerCase().includes (this.filtroPorTexto.toLowerCase())
+      })
+  }
+
+  filtrarContatosPorLetraInicial (letra:string) : Contato[] {
+      return this.filtrarContatosPorTexto().filter(contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+      } )
+  }
 }
---
+++ src/app/componentes/contato/contato.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  imports: [],
+  templateUrl: './contato.component.html',
+  styleUrl: './contato.component.css'
+})
+
+export class ContatoComponent {
+  @Input() nome: string = ''
+  @Input() telefone: string = ''
+}
